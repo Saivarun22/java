e7fccc2cd103eeb10949b73f31eae9b3af666746
Saivarun22 <149492357+Saivarun22@users.noreply.github.com>
2026-09-11 16:13:50 +0530

    Add files via upload

--- FunctionalInterface.java
+++ FunctionalInterface.java
@@ -1,34 +1,51 @@
 interface A{
 
      int execution (int i);
-     double Practice(double y);
+     
 }
 public class FunctionalInterface {
     public static void main(String [] args){
 
     
 
-    A obj  = new A(){
-
-         public int execution(int i){
-            int res= i*i;
-            return res;
-            
-         }
-
-         public double Practice(double y){
-            double res=y*y;
-            return res;
-            
-         }
-
-    
-
-    };
+    A obj  = (i) -> i*i;
 
    
-    System.out.println("Execution result:" + obj.execution(7));
-    System.out.println("Practice result:" + obj.Practice(9));
+    obj.execution(7);
     
+ }
 }
-}
+// interface A{
+
+//      int execution (int i);
+//      double Practice(double y);
+// }
+// public class FunctionalInterface {
+//     public static void main(String [] args){
+
+    
+
+//     A obj  = new A(){
+
+//          public int execution(int i){
+//             int res= i*i;
+//             return res;
+            
+//          }
+
+//          public double Practice(double y){
+//             double res=y*y;
+//             return res;
+            
+//          }
+
+    
+
+//     };
+
+   
+//     System.out.println("Execution result:" + obj.execution(7));
+//     System.out.println("Practice result:" + obj.Practice(9));
+    
+// }
+// }
