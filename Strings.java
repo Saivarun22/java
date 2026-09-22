d4a28a35240b95dace97ed4bcae49b87bd8afb70
Saivarun22 <149492357+Saivarun22@users.noreply.github.com>
2026-09-22 12:22:35 +0530

    Add files via upload

--- Strings.java
+++ Strings.java
@@ -1,92 +1,50 @@
-// import java.util.*;
-// public class Strings{
-//     public static void main(String args[]){
-//         String name="sai";
-//         name = name + "Varun";
-
-//         String name1="Sai";
-//         String name2="Sai";
-
-//         System.out.println(name1 == name2);
-//     }
-// 
-
-// class Mobile{
-//     String brand;
-//     int price;
-//     static String type;
-     
-//     public void command(){
-//     System.out.println(brand +" : "+ price +" : "+type);
-//     }
-//     public static void command1(Mobile phone){
-//         System.out.println(phone.brand +" : "+ phone.price +" : "+type);
-//     }
-// }
-
-// public class Strings{
-    
-//     public static void main(String args[]){
-
-//         Mobile phone1 = new Mobile();
-//         phone1.brand="Realmi";
-//         phone1.price=35000;
-//         Mobile.type="smart phone";
-
-//         Mobile phone2 = new Mobile();
-//         phone2.brand="samsung";
-//         phone2.price=65000;
-//         Mobile.type="smart phone";
-        
-//         Mobile phone3 = new Mobile();
-//         phone3.brand="One plus";
-//         phone3.price=80000;
-//         Mobile.type="smart phone";
-
-//         phone1.type="phone";
-
-//         phone1.command();
-//         phone2.command();
-//         phone3.command();
-//         Mobile.command1(phone3);
-        
-
-
-//     }
-// }
-
-class Mobile{
-    String brand;
-    int price;
-    static String type;
-
-    public Mobile(){
-        brand="apple";
-        price=75000;
-        System.out.println("it in constructor");
-    }
-
-    static{
-        type="Mobile";
-        System.out.println("it in  static method");
-    }
-     
-    public void command(){
-    System.out.println(brand +" : "+ price +" : "+type);
-    }
-    
-}
+import java.util.*;
 public class Strings{
-    
     public static void main(String args[]){
+        
+        
+        String str ="Java"; //Stored in StringConstantPool
+        String str2 ="Java";//Stored in StringConstantPool
+        String  s1 = new String("Java");//Sored in HeapArea
+        String  s2 = new String("Java");//Sored in HeapArea
+        boolean a = str==str2;
+        System.out.println(a);//True//Here the Result is True because of "==" Will Compare The Reference of String is Same or Not
+        boolean b = str2==s1;
+        System.out.println(b);//False
+        boolean c = s1==s2;
+        System.out.println(c);//False
 
-        Mobile phone1 = new Mobile();
-        phone1.brand="Realmi";
-        phone1.price=35000;
-        Mobile.type="smart phone";
+        boolean d = str.equals(str2);
+        System.out.println(d);//True//Here the Result is True because of "Equals()" Will Compare The Data of String is Same or Not
+        boolean e = str2.equals(s1);
+        System.out.println(e);//True
+        boolean f = s1.equals(s2);
+        System.out.println(f);//Tuue
 
-        Mobile phone2 = new Mobile();
 
+        String sr ="Java"; //Stored in StringConstantPool
+        String sr2 ="Java";//Stored in StringConstantPool
+        String sr3 = sr+sr2; //Stored in HeapArea Because of sr3 Refers to Sr concats sr2 so it will Create in HeapArea
+        String sr4 = "Ja"+"va";//Stored in StringConstantPool
+        String  st1 = new String("Java");//Sored in HeapArea
+        String  st2 = new String("Java");//Sored in HeapArea
+        boolean g = sr2==sr3;
+        System.out.println(g);//False;
+        boolean h = sr2==sr4;
+        System.out.println(h);//True
+        boolean i = st1==st2;
+        System.out.println(i);//False
+
+        boolean j = sr2.equals(sr3);
+        System.out.println(j);//False
+        boolean k = sr2.equals(sr4);
+        System.out.println(k);//True
+        boolean l = s1.equals(s2);
+        System.out.println(l);//True
+        boolean m = sr2.equals(st1);
+        System.out.println(m);//True
+        boolean n = sr2.equals(st2);
+        System.out.println(n);//True
 
     }
 }
